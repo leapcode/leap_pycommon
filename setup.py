58e6a1df85204ab30b9a9351de32ac2f71f1026b
drebs <drebs@leap.se>
2013-03-31 21:02:23 -0300

    Add events mechanism dependencies to setup.py file.

--- setup.py
+++ setup.py
@@ -19,8 +19,17 @@ setup file for leap.common
 """
 from setuptools import setup, find_packages
 
+
 requirements = [
+    'protobuf',
+    'protobuf.socketrpc',
 ]
+
+
+dependency_links = [
+    'https://protobuf-socket-rpc.googlecode.com/files/protobuf.socketrpc-1.3.2-py2.6.egg#egg=protobuf.socketrpc',
+]
+
 
 # XXX add classifiers, docs
 
@@ -39,5 +48,6 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     #test_suite='leap.common.tests',
-    #install_requires=requirements,
+    install_requires=requirements,
+    dependency_links=dependency_links,
 )
